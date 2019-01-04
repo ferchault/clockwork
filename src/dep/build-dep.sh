8277c0e83666ba820ee362f9d73d38004fc84b52
Guido <guido@vonrudorff.de>
2019-01-04 14:17:10 +0100

    Added makefile and dependency builder

---
+++ src/dep/build-dep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir dist
+
+tar xzf openbabel-2.4.1.tar.gz
+cd openbabel-2.4.1/
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=../../dist -DWITH_INCHI=OFF
+make -j4
+make install
+cd ../..
+
