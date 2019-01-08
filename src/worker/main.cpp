a0371df263339f8157e6a21888e941e91e16a5dd
Guido <guido@vonrudorff.de>
2019-01-08 18:25:20 +0100

    Communication protocol with redis

--- src/worker/main.cpp
+++ src/worker/main.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <stdlib.h>
 #include <omp.h>
+#include <ctime>
+#include <cstring>
 //#include <openbabel/mol.h>
 //#include <openbabel/forcefield.h>
 //#include <openbabel/obconversion.h>
@@ -22,19 +24,69 @@ public:
 	unsigned int * frozen_dihedrals_n;
 	unsigned int * frozen_dihedrals_i;
 
-	void read_binary(std::string);
+	void read_binary(unsigned char *){};
 };
 
 redisContext * redis_connect() {
-	redisContext * c = redisConnect(getenv("CONFORMERREDIS"), 6379);
-	if (c != NULL && c->err) {
-		printf("Error: %s\n", c->errstr);
+	redisContext * context = redisConnect(getenv("CONFORMERREDIS"), 6379);
+	if (context != NULL && context->err) {
+		printf("Error: %s\n", context->errstr);
 	}
 	redisReply *reply;
-	reply = (redisReply*)redisCommand(c, "AUTH chemspacelab");
+	reply = (redisReply*)redisCommand(context, "AUTH chemspacelab");
 	freeReplyObject(reply);
 
-	return c;
+	return context;
+}
+
+// reads a new work package from redis securely (i.e. without chance of dropping the package)
+int redis_fetch(redisContext * context, unsigned char * payload, size_t * payloadsize) {
+	redisReply *reply;
+	reply = (redisReply*)redisCommand(context, "RPOPLPUSH WPQ WPR");
+	if (reply == NULL || reply->type != REDIS_REPLY_STRING) {
+		// invalid reply || no further work or server-side issue
+		freeReplyObject(reply);
+		return 2;
+	}
+
+	// valid reply with more work
+	*payloadsize = reply->len;
+	unsigned char * chunk = new unsigned char[reply->len];
+	std::memcpy(chunk, reply->str, reply->len);
+	payload = chunk;
+	freeReplyObject(reply);
+
+	// store start time 
+	reply = (redisReply*)redisCommand(context, "HSET WPRstarted %b %d", payload, payloadsize, (int)std::time(NULL));
+	if (reply == NULL || reply->type != REDIS_REPLY_INTEGER) {
+		// unexpected result, terminating to be on the safe side
+		freeReplyObject(reply);	
+		return 3;
+	}
+	freeReplyObject(reply);
+	return 0;
+}
+
+// clears up the redis queues with running work packages
+int redis_notify(redisContext * context, unsigned char * payload, size_t payloadsize) {
+	redisReply *reply;
+	
+	// delete entry for running work packages
+	reply = (redisReply*)redisCommand(context, "LREM WPR 1 %b", payload, payloadsize);
+	if (reply == NULL) {
+		std::cout << "Lost connection to REDIS in redis_notify LREM." << std::endl;
+		return 2;
+	}
+	freeReplyObject(reply);
+
+	// delete age entry
+	reply = (redisReply*)redisCommand(context, "HDEL WPRstarted %b", payload, payloadsize);
+	if (reply == NULL) {
+		std::cout << "Lost connection to REDIS in redis_notify HDEL." << std::endl;
+		return 2;
+	}
+
+	return 0;	
 }
 
 int main(int argc,char **argv)
@@ -42,10 +94,15 @@ int main(int argc,char **argv)
 	Archive archive;
 	archive.read_archive("../../fixtures/sample.archive");
 	
-	redisContext *c = redis_connect();
+	redisContext *context = redis_connect();
 
-
-	/*
+	Workpackage *wp = new Workpackage();
+	unsigned char * payload = 0;
+	size_t payloadsize;
+	while (redis_fetch(context, payload, &payloadsize)) {
+		wp->read_binary(payload);
+		
+/*
 	// Needed such that openbabel does not try to parallelise
 	omp_set_num_threads(1);
 
@@ -80,6 +137,13 @@ int main(int argc,char **argv)
 		// Use mol.GetCoordinates() for rmsd check
 	}
   */
-	redisFree(c);
+		if (!redis_notify(context, payload, payloadsize)) {
+			std::cout << "Problems in communicating to REDIS. Aborting." << std::endl;
+		}
+
+		delete[] payload;
+	}
+
+	redisFree(context);
 	return 0;
 }
