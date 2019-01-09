43c720aacb38774425c70c3349be8e47955a1902
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-01-09 17:27:13 +0100

    proof-of-concept worker

---
+++ src/worker/poc_conformations.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <omp.h>
+#include <openbabel/mol.h>
+#include <openbabel/forcefield.h>
+#include <openbabel/obconversion.h>
+
+#include "io.cpp"
+#include "kabsch.h"
+#include "molecule.cpp"
+
+int main(int argc,char **argv)
+{
+	Archive archive;
+	archive.read_archive("../../fixtures/sample.archive");
+
+	unsigned int idx = archive.molecule_ids[0];
+
+	example_worker(archive, 0);
+
+	return 0;
+}
