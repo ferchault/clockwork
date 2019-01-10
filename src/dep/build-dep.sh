eb80ddfbd0d5568925065c2ce6e46b86349f10fb
Guido <guido@vonrudorff.de>
2019-01-10 12:07:20 +0100

    More dependencies

--- src/dep/build-dep.sh
+++ src/dep/build-dep.sh
@@ -16,3 +16,11 @@ cd hiredis-master
 PREFIX=../dist make
 PREFIX=../dist make install
 
+
+wget http://www.netlib.org/lapack/lapack-3.8.0.tar.gz
+tar xzf lapack-3.8.0.tar.gz
+cd lapack-3.8.0/
+cp INSTALL/make.inc.gfortran make.inc
+make lapacklib lapackelib
+
+module load OpenBLAS/0.3.3-GCC-8.2.0-2.31.1 ScaLAPACK/2.0.2-gompi-2018.08-OpenBLAS-0.3.3
