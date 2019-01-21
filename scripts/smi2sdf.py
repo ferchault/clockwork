ffafbb82b0d2461ecba3d005023b05b61f40ce6d
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-01-21 12:25:44 +0100

    space

--- scripts/smi2sdf.py
+++ scripts/smi2sdf.py
@@ -1,15 +1,25 @@
+"""
 
+convert smiles to sdf
+
+"""
 import rdkit
 import rdkit.Chem as Chem
 import rdkit.Chem.AllChem as AllChem
 
 def main():
+    """
+
+    convert smiles to sdf
+
+    """
 
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--smiles', type=str, help='', metavar='SMI')
     parser.add_argument('-f', '--filename', type=str, help='Output file', metavar='FILE')
-    parser.add_argument('-n', '--number', type=str, help='Number of conformers', metavar='int', default=1)
+    parser.add_argument('-n', '--number', type=str,
+                        help='Number of conformers', metavar='int', default=1)
     args = parser.parse_args()
 
 
