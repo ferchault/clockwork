6d31d0994a7cee7df3f51242a991690c3bb29cbf
Guido <guido@vonrudorff.de>
2019-02-21 13:53:17 +0100

    Redis example worker

---
+++ src/worker/redis_worker.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import time
+import os
+import redis
+import sys
+
+class Taskqueue(object):
+	def __init__(self, connectionstring, projectprefix):
+		""" Connection string: password@host:6380/1 """
+		self._prefix = projectprefix
+		password, rest = connectionstring.split('@')
+		hostname, rest = rest.split(':')
+		port, db = rest.split('/')
+		try:
+			self._con = redis.Redis(host=hostname, port=int(port), db=int(db), password=password)
+		except:
+			raise ValueError('Unable to connect to this redis instance.')
+
+	def _start_task(self):
+		""" Fetches a new task from redis and returns the associated string."""
+		task = self._con.rpoplpush(self._prefix + '_Queue', self._prefix + '_Running')
+		if task is None:
+			return None
+		self._starttime = time.time()
+		self._con.hset(self._prefix + '_Started', task, self._starttime)
+		return task
+		
+	def _store_result(self, taskstring, resultstring, logmessage=None):
+		""" Signals task completion to redis and stores the results."""
+		# remove server-side backup
+		self._con.lrem(self._prefix + '_Running', 1, taskstring)
+		self._con.hdel(self._prefix + '_Started', taskstring)
+
+		# Store results and logs
+		self._con.lpush(self._prefix + '_Results', resultstring)
+		if logmessage is not None:
+			self._con.hset(self._prefix + '_Log', taskstring, logmessage)
+
+		# Update stats
+		unixminute = int(time.time() / 60)
+		duration = time.time() - self._starttime
+		statskey = self._prefix + '_Stats:%d' % unixminute
+		self._con.rpush(statskey, duration)
+		self._con.expire(statskey, 3600)
+	
+	def _get_deadline(self):
+		""" Calculates the deadline of job termination. Returns a unix time stamp. When run outside SLURM, returns 0. """
+		return 0
+
+	def main_loop(self, callback):
+		deadline = self._get_deadline()
+		while True:
+			if deadline > 0 and deadline - time.time() < 120:
+				break
+			task_str = self._start_task()
+			if task_str is None:
+				break
+			result_str, log_str = callback(task_str)
+			self._store_result(task_str, result_str, log_str)
+
+	def insert(self, taskstring):
+		""" Enqueues a task. """
+		self._con.lpush(self._prefix + '_Queue', taskstring)
+
+def do_work(task):
+	""" Sample task evaluation. Returns a result as string and an optional log message."""
+	time.sleep(2)
+	return "result", "Another information"
+
+if __name__ == '__main__':
+	tasks = Taskqueue(os.getenv('CHEMSPACELAB_REDIS_CONNECTION'), 'DEBUG')
+	for i in range(3):
+		tasks.insert('test%d' % i)
+	try:
+		tasks.main_loop(do_work)
+	except:
+		# Ugly catch-all handler: no SLURM log file wanted!
+		sys.exit(1)
