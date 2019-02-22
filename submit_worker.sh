9d33636e0bb273cc71a53609724740a1a472183b
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-02-22 12:19:47 +0100

    added slurm header

--- submit_worker.sh
+++ submit_worker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=exconwork
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --error=/dev/null
+#SBATCH --output=/dev/null
+#SBATCH --time=00:30:00
+#SBATCH --time-min=00:05:00
+#SBATCH --partition=WAT
+#SBATCH --no-requeue
 
 connection=$1
 
