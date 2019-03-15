6db8b3fb23e6d3e73a94ca7a12508ed8bf3d60fe
von Rudorff, Guido Falk <guido@vonrudorff.de>
2019-03-15 15:00:28 +0100

    Reflect API change

--- src/maint/refill-alchemy.sh
+++ src/maint/refill-alchemy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+PROJECT="$1"
 # connect to REDIS
 . /home/vonrudorff/opt/conda/install/etc/profile.d/conda.sh
 conda activate /home/vonrudorff/opt/conda/rdkit
 connection=$(cat /home/vonrudorff/.redis-credentials)
-CHEMSPACELAB_REDIS_CONNECTION="$connection" python $1/../worker/rediscomm.py --haswork PRODUCTION || exit
+CHEMSPACELAB_REDIS_CONNECTION="$connection" python /home/vonrudorff/workcopies/clockwork/src/worker/rediscomm.py --haswork $PROJECT || exit
 
 # work left, let's submit
 CURRENT=$(/home/admin/slurm/slurm16/bin/squeue -u $(whoami) | grep exconwo | wc -l)
-TARGET=2
+TARGET=200
 DIFF=$(($TARGET-CURRENT))
+#echo $DIFF
 [ $DIFF -lt 0 ] && DIFF=0
 
 for i in $(seq 1 $DIFF)
 do
-	/home/admin/slurm/slurm16/bin/sbatch $1/worker-alchemy.sh &> /dev/null
+	/home/admin/slurm/slurm16/bin/sbatch <(cat /home/vonrudorff/workcopies/clockwork/src/maint/worker-alchemy.sh | sed "s/PROJECTPROJECTPROJECT/$PROJECT/") &> /dev/null
 done
--- src/maint/worker-alchemy.sh
+++ src/maint/worker-alchemy.sh
@@ -18,5 +18,5 @@ connection=$(cat /home/vonrudorff/.redis-credentials)
 python3 /home/vonrudorff/workcopies/clockwork/src/worker/worker.py \
         -f /home/vonrudorff/workcopies/qm9-C7O2H10/full.sdf.gz \
         --torsions-file /home/vonrudorff/workcopies/qm9-C7O2H10/list_torsions_idx \
-        --connect-redis "$connection"
+        --redis-connect "$connection" --redis-task PROJECTPROJECTPROJECT
 
