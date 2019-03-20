cce3cd3c160362daf068a01ef76e51fcb087a998
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-03-20 14:43:42 +0100

    the energies

--- src/worker/adm_merge.py
+++ src/worker/adm_merge.py
@@ -4,6 +4,9 @@ merge jobs
 
 """
 
+import rmsd
+
+import pickle
 import gzip
 
 import numpy as np
@@ -11,6 +14,16 @@ import scipy.special
 
 import adm_fchl
 import adm_molecule
+
+def save_obj(name, obj):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
 
 def costfunc(clockres, n):
     """
@@ -52,6 +65,9 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
     atoms = np.array(atoms)
     n_atoms = len(atoms)
 
+    if len(jobs) == 0:
+        return []
+
     if type(jobs[0]) == str:
         jobs = [info(job) for job in jobs]
 
@@ -70,21 +86,38 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
         energies.append(energy)
         costlist.append(cost)
 
-
-    costlist = np.array(costlist)
-    energies = np.round(energies, decimals=decimals)
-
-    if only_energies is None:
-        unique_energies = np.unique(energies)
-    else:
-        unique_energies = only_energies
-
-    if debug:
-        print("merge: found {:} energies".format(len(unique_energies)))
+        if debug:
+            print("in", job[:5])
 
     keep = []
     keep_origin = []
     keep_merge = []
+
+    costlist = np.array(costlist)
+    energies = np.round(energies, decimals=decimals)
+
+    # Only do fchl on unique energies, pass others to keep_list
+    if only_energies is None:
+        unique_energies = np.unique(energies)
+    else:
+        unique_energies = np.unique(energies)
+
+        for energy in unique_energies:
+
+            # exists energy in only_energies
+            idx, = np.where(only_energies == energy)
+            if len(idx) > 0: continue
+
+            jobidx, = np.where(energies == energy)
+            for idx in jobidx:
+                keep.append(idx)
+                keep_origin.append(idx)
+                keep_merge.append(0)
+
+        unique_energies = only_energies
+
+    if debug:
+        print("merge: {:} energies".format(len(unique_energies)), unique_energies)
 
     for energy in unique_energies:
 
@@ -100,6 +133,9 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
 
         costs = costlist[jobidx]
 
+        if debug:
+            print(energy, costs)
+
         positions = []
         for i in jobidx:
             position = jobs[i][-1]
@@ -107,15 +143,11 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
             position = position.reshape((n_atoms, 3))
             positions.append(position)
 
-
         if debug:
             print("merge: qml for {:} @ {:}".format(len(jobidx), energy))
 
         # Energy are the same, compare with FCHL
-        representations = adm_fchl.get_representations_fchl(atoms, positions)
-        representations = np.array(representations)
-
-        similarity = adm_fchl.get_kernel_fchl(representations, representations)
+        similarity = adm_fchl.get_similarity(atoms, positions)
 
         dealtwith = []
 
@@ -123,25 +155,31 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
 
             if j in dealtwith: continue
 
-            rowsim = np.argwhere(row >= threshold)
-            rowsim = rowsim.flatten()
-            rowcosts = costs[rowsim]
+            rowsim, = np.where(row >= threshold)
 
-            # find the lowest cost, of all (incl dealtwith)
-            costidx = np.argsort(rowcosts)
-            keep_sim_cost = costidx[0]
+            # if the molecules are broken, fchl will give similarity of nan
+            # just skip these molecules
+            if len(rowsim) == 0: continue
+
+            # find the lowest row cost, of all (incl dealtwith)
+            rowcosts = costs[rowsim]
+            costidx = np.argsort(rowcosts)[0]
+            keep_sim_cost = rowsim[costidx]
 
             # want not dealt with index and lowest cost
             rowsim = [idx for idx in rowsim if idx not in dealtwith]
-            costs_pos = costs[rowsim]
-            costs_pos_low = np.argsort(costs_pos)[0]
-
-            keep_sim_pos = rowsim[costs_pos_low]
+            rowcosts = costs[rowsim]
+            costidx = np.argsort(rowcosts)[0]
+            keep_sim_pos = rowsim[costidx]
 
             # convert similarity idx to jobidx
             keep_idx = jobidx[keep_sim_pos]
             keep_cost = jobidx[keep_sim_cost]
 
+            if debug:
+                print("keep", keep_idx, keep_cost, costlist[keep_cost])
+
+            # Save the indexes
             keep.append(keep_idx)
             keep_origin.append(keep_cost)
             keep_merge.append(len(rowsim) - 1)
@@ -157,19 +195,18 @@ def energyandfchl(jobs, atoms, decimals=1, threshold=0.98, reset_count=False, on
         job = jobs[jidx]
         job = list(job)
 
+        if debug:
+            print("rtn", jidx, job[:5])
+
         if jidx != oidx:
 
             ojob = jobs[oidx]
             origin = ojob[2]
             job[2] = origin
 
-        if reset_count:
-
-            job[3] = nmerge
-
-        else:
-
-            job[3] += nmerge
+        # merge count
+        if reset_count: job[3] = nmerge
+        else: job[3] += nmerge
 
         rtnjobs.append(job)
 
@@ -238,7 +275,7 @@ def main():
     parser.add_argument('-s', '--sdf', type=str, help='', metavar='file')
     args = parser.parse_args()
 
-    with open(args.filename) as f:
+    with open(args.filename, 'r') as f:
         lines = f.readlines()
 
 
@@ -257,6 +294,54 @@ def main():
 
         print("unknown format")
         quit()
+
+
+    lines_molid = []
+
+    try:
+        lines_molid = load_obj("tmp_molid")
+    except FileNotFoundError:
+
+        for i, line in enumerate(lines):
+
+            line = eval(line)
+            molid = line[0]
+            lines_molid.append(molid)
+
+        save_obj("tmp_molid", lines_molid)
+
+    lines_molid = np.array(lines_molid)
+    unique_molid = np.unique(lines_molid)
+
+    for molid in unique_molid:
+
+        # Create mol
+        mol = moldb[molid]
+
+        atoms = [atom for atom in mol.GetAtoms()]
+        atoms_str = [atom.GetSymbol() for atom in atoms]
+        atoms_int = [atom.GetAtomicNum() for atom in atoms]
+
+        # find all lines with for mol
+        lineidx, = np.where(lines_molid == molid)
+
+        print(lineidx)
+
+        resultlist = []
+        for i in lineidx:
+            resultlist.append(eval(lines[i]))
+
+        # Merge results
+        results = energyandfchl(resultlist, atoms_int)
+
+        for result in results:
+            print(result[:3])
+
+        # print("merged mol", molid, len(lineidx), "->", len(results))
+
+
+
+    quit()
 
 
     # Select mol and find atoms
