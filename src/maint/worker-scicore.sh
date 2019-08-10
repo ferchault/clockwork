1d387f538239cdb10238b23d0599d775795509ad
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-08-10 12:53:07 +0200

    slurm worker

--- src/maint/worker-scicore.sh
+++ src/maint/worker-scicore.sh
@@ -1,25 +1,20 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=exconwork
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --error=/dev/null
-#SBATCH --output=/dev/null
+#SBATCH --error=_%j.stderr
+#SBATCH --output=_%j.stdout
 #SBATCH --time=00:30:00
 #SBATCH --time-min=00:05:00
 #SBATCH --qos=30min
 
-module load intel/2019.01
-module load Anaconda3/5.0.1
+HERE=/scicore/home/lilienfeld/bexusi36/dev/2019-clockwork/clockwork
 
-export OMP_NUM_THREADS=1
+cd $HERE
 
-source activate /scicore/home/lilienfeld/rudorff/opt/conda/rdkit
-connection=$(cat /scicore/home/lilienfeld/rudorff/.redis-credentials)
-export PYTHONPATH=/scicore/home/lilienfeld/rudorff/.local/lib/python3.7/site-packages:$PYTHONPATH
+PYTHON=src/worker/env/bin/python
 
-python3 /scicore/home/lilienfeld/rudorff/workcopies/clockwork/src/worker/worker.py \
-        -f /scicore/home/lilienfeld/bexusi36/qm9-C7O2H10/full.sdf.gz \
-        --torsions-file /scicore/home/lilienfeld/bexusi36/qm9-C7O2H10/list_torsions_idx \
-        --redis-connect "$connection" --redis-task PROJECTPROJECTPROJECT
+$PYTHON -u src/worker/worker.py --sdf $HOME/db/example_pentane_nosym.sdf --redis-task edge
+
 
