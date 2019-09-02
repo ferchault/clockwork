837309532aef171dc9548e7723872cc415b971d3
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-09-02 14:13:57 +0200

    array from guido

--- src/maint/worker-scicore.sh
+++ src/maint/worker-scicore.sh
@@ -8,6 +8,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --time-min=00:05:00
 #SBATCH --qos=30min
+#SBATCH --array=1-100000
 
 HERE=/scicore/home/lilienfeld/bexusi36/dev/2019-clockwork/clockwork
 
@@ -16,6 +17,13 @@ cd $HERE
 PYTHON=src/worker/env/bin/python
 
 # $PYTHON -u src/worker/worker.py --sdf $HOME/db/example_pentane_nosym.sdf --redis-task hello
-$PYTHON -u src/worker/worker.py --sdf $HOME/db/edgecase_1.sdf --redis-task edge
 
+SDF=$HOME/db/qm9.c7o2h10.sdf.gz
+SDFTOR=$HOME/db/qm9.c7o2h10.torsions
+TASK=case1
+$PYTHON -u src/worker/worker.py --sdf $SDF --sdftor $SDFTOR --redis-task $TASK
+
+# SDF=$HOME/db/edgecase_1.sdf
+# TASK=case1
+# $PYTHON -u src/worker/worker.py --sdf $HOME/db/edgecase_1.sdf --redis-task edge
 
