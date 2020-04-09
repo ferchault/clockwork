10c4084582c5fd4f31ebb2728a2222a7f6570ec6
Guido <guido@vonrudorff.de>
2020-04-09 10:57:35 +0200

    Print ranking

--- scripts/costorder.py
+++ scripts/costorder.py
@@ -20,13 +20,36 @@ def combinations_in_group(T, R):
 def group_cost(combinations):
 	cost = 0
 	for case in combinations:
-		cost += np.prod(map(configuration_count, case))
+		cost += np.prod(list(map(configuration_count, case)))
 	return cost
 
 RS = 5
-print ('     ' + ' '.join(['  R:%6d' % _ for _ in range(RS)]))
-for T in range(1, 5):
+print ('     ' + ' '.join(['  R:%16d' % _ for _ in range(RS)]))
+
+specifications = []
+costs = []
+for T in range(1, 7):
 	res = []
 	for R in range(RS):
-		res.append(scs.binom(N, T)*group_cost(combinations_in_group(T, R)))
-	print ('T:%3d' % T + ' '.join(['%10d' % _ for _ in res]))
+		cost = scs.binom(N, T)*group_cost(combinations_in_group(T, R))
+		res.append(cost)
+		costs.append(cost)
+		specifications.append((T, R))
+	print ('T:%3d' % T + ' '.join(['%20d' % _ for _ in res]))
+
+ranks = sorted(costs)
+print ('     ' + ' '.join(['  R:%16d' % _ for _ in range(RS)]))
+for T in range(1, 7):
+	res = []
+	for R in range(RS):
+		cost = scs.binom(N, T)*group_cost(combinations_in_group(T, R))
+		res.append(ranks.index(cost))
+	print ('T:%3d' % T + ' '.join(['%20d' % _ for _ in res]))
+
+
+print ("Order: nbody, resolution")
+labels = []
+for pos in np.argsort(costs):
+	nbody, resolution = specifications[pos]
+	labels.append(f"N{nbody}R{resolution}")
+print (' '.join(labels))
