625af16f5d39164fd0fb0ba6a2ab7245e9f01e9e
Guido von Rudorff <guido@vonrudorff.de>
2020-06-03 09:14:19 +0200

    Nicer scriptable interface

--- scripts/generate-workpackages.py
+++ scripts/generate-workpackages.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import sys
 import itertools as it
+import argparse
 
 # arguments
-molname, bucket, dihs = sys.argv[1:]
-dihs = dihs.split("-")
-nbody, resolution = bucket.split("R")
-nbody = int(nbody[1:])
+parser = argparse.ArgumentParser(description="builds a clockwork batch input file")
+parser.add_argument("molname", type=str)
+parser.add_argument("buckets", type=str)
+parser.add_argument("dihedrals", type=str)
 
-# output
-for wp in it.combinations(dihs, nbody):
-	print (f"{molname}:" + '-'.join(wp) + ":" + resolution) 
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    dihs = args.dihedrals.split("-")
+    for bucket in args.buckets.split():
+        nbody, resolution = bucket.split("R")
+        nbody = int(nbody[1:])
+        for wp in it.combinations(dihs, nbody):
+            print(f"{args.molname}:" + "-".join(wp) + ":" + resolution)
