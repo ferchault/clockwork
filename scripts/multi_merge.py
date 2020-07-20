2d9c6c67ee037820f647bc801a59c742f23da8c6
Dom1L <lemm.d@web.de>
2020-07-20 16:59:34 +0200

    direct path to merged files

--- scripts/multi_merge.py
+++ scripts/multi_merge.py
@@ -20,7 +20,7 @@ def batch_inp(mol_path, batch_num, wp_path):
     batches = []
     for mol in tqdm(mol_path):
         mol_name = os.path.dirname(mol).split('/')[-1]
-        p_merge = f'{wp_path}/merge_files/batch{batch_num-1}/{mol_name}_batch{batch_num-1}.merged'
+        p_merge = f'{wp_path}/batch{batch_num-1}/{mol_name}_batch{batch_num-1}.merged'
         if not os.path.isfile(p_merge) and batch_num > 1:
             print(f'Batch {batch_num-1} of molecule {mol_name} has not been merged yet. Skipped.')
             continue
@@ -37,10 +37,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--molpath', action='store', help='', metavar="dir")
     parser.add_argument('--outpath', action='store_true', help='')
-    parser.add_argument('--ncpus', action='store', help='', metavar="file")
+    parser.add_argument('--ncpus', action='store', help='', metavar="o")
     parser.add_argument('--nbatch', action='store', metavar="int", type=int)
 
     args = parser.parse_args()
+    # python multi_merge.py --molpath /mnt/FOXTROT/CONFORMER/merge_files2 --outpath /mnt/FOXTROT/CONFORMER/merge_files2/ --ncpus 20 --nbatch 1
 
     outpath = f'{args.outpath}/batch{args.nbatch}'
     os.makedirs(outpath, exist_ok=True)
@@ -52,6 +53,4 @@ if __name__ == '__main__':
         with tqdm(total=len(b), desc='Molecules') as pbar:
             for _ in pool.imap_unordered(merge_batch, b):
                 pbar.update()
-
-
 
